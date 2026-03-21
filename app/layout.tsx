91b1fd8bfb77df5cf25042486acb82270e24a078
muatroi29-cpu <muatroi29@gmail.com>
2026-03-21 21:14:43 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-light.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon-dark.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
